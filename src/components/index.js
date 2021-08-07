c113a3ebecac1be392f1342120e4e657d0ea5462
Vithujan19 <vithusundar@gmail.com>
2021-08-07 20:09:25 +0530

    first update

--- src/components/index.js
+++ src/components/index.js
@@ -1,19 +1,87 @@
 import React from 'react';
-import './index.css';
+// import './index.css';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import {Tabs,Paper} from '@material-ui/core/';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
 
-function index() {
-    return (
-        <div className="bodyClass">
-                <h2>Details</h2>
-                <div className="tab">
-                    <div className="tab">
-                        <button className="tablinks" onclick="openCity(event, 'invoice-details')" id="defaultOpen">Invoice</button>
-                        <button className="tablinks" onclick="openCity(event, 'Product')">Product</button>
-                        <button className="tablinks" onclick="openCity(event, 'Reports')">Reports</button>
-                    </div>
-                </div>
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
 
-                <div id="invoice-details" className="tabcontent">
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`vertical-tabpanel-${index}`}
+      aria-labelledby={`vertical-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box p={3}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `vertical-tab-${index}`,
+    'aria-controls': `vertical-tabpanel-${index}`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.paper,
+    display: 'flex',
+    height: 224,
+  },
+  tabs: {
+    borderRight: `1px solid ${theme.palette.divider}`,
+  },
+}));
+
+function Index() {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (<div>
+
+     <h1 style={{display:"block", padding:10}}> Details</h1>
+    <div className={classes.root} >
+       
+
+      <Tabs
+        orientation="vertical"
+        variant="scrollable"
+        value={value}
+        onChange={handleChange}
+        aria-label="Vertical tabs example"
+        className={classes.tabs}
+      >
+        <Tab label="Invoice" {...a11yProps(0)} />
+        <Tab label="Product" {...a11yProps(1)} />
+        <Tab label="Reports" {...a11yProps(2)} />
+        
+      </Tabs>
+      <TabPanel value={value} index={0} style={{width:"100%"}}>
+          <Paper elevation={3} style={{padding:20}}>
+      <div id="invoice-details" className="tabcontent">
                     <form action="/action_page.php">
                         <div className="container">
                             <form action="#" method="get">
@@ -48,19 +116,24 @@ function index() {
                                     </tr>
 
                                 </table>
-                                <table border="1" style={{width: "60%"}}>
+                                <table border="1" style={{width: "90%"}}>
                                     <tr>
                                         <td>TOTAL :<input type="number" placeholder="Total" name="total" id="total" /> </td>
                                         <td><button>Process</button></td>
                                         <td><button>Clear</button></td>
                                     </tr>
                                 </table>
+                                
                             </form>
                         </div>
+                        
                     </form>
                 </div>
-
-                <div id="Product" className="tabcontent">
+</Paper>
+      </TabPanel>
+      <TabPanel value={value} index={1}  style={{width:"100%"}}>
+      <Paper elevation={3} style={{padding:20}}>
+      <div id="Product" className="tabcontent">
                     <h3>Product</h3>
                     <ul>
                         <li><label for="Product ID">Product ID: <input type="text" placeholder="Product ID" name="Product ID" id="Product ID" /></label></li>
@@ -79,8 +152,11 @@ function index() {
                         </tr>
                     </table>
                 </div>
-
-                <div id="Reports" className="tabcontent">
+                </Paper>
+      </TabPanel>
+      <TabPanel value={value} index={2}  style={{width:"100%"}}>
+      <Paper elevation={3} style={{padding:20}}>
+      <div id="Reports" className="tabcontent">
                     <h3>Reports</h3>
                     <form action="/action_page.php">
                         <div className="container">
@@ -103,7 +179,7 @@ function index() {
                                         <td><button>Search</button></td>
                                     </tr>
                                 </table>
-                                <table border="1" style={{width: "60%"}}>
+                                <table border="1" style={{width: "90%"}}>
                                     <thead>
                                         <tr>
                                             <th>Invoice No</th>
@@ -131,8 +207,24 @@ function index() {
                         </div>
                     </form>
                 </div>
-        </div >
-    )
+                </Paper>
+      </TabPanel>
+      {/* <TabPanel value={value} index={3}>
+        Item Four
+      </TabPanel>
+      <TabPanel value={value} index={4}>
+        Item Five
+      </TabPanel>
+      <TabPanel value={value} index={5}>
+        Item Six
+      </TabPanel>
+      <TabPanel value={value} index={6}>
+        Item Seven
+      </TabPanel> */}
+    </div>
+    </div>
+  );
 }
 
-export default index;
+
+export default Index;
